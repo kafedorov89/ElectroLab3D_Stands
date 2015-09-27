5adbda52a4d19f053362d43f5b321865476ef07f
kafedorov89 <kafedorov89@gmail.com>
2015-09-28 00:23:08 +0500

    Start works

---
+++ StandNavigation/Assets/StandNav.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StandNav : MonoBehaviour {
+
+	public float step = 10f;
+	public float smoothing = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.W)){
+			// Create a postion the camera is aiming for based on 
+			// the offset from the target.
+			Vector3 newCamPos = transform.position + Vector3.forward * step;
+			
+			// Smoothly interpolate between the camera's current 
+			// position and it's target position.
+			transform.position = Vector3.Lerp(transform.position, 
+			                                   newCamPos,   
+			                                   smoothing * Time.deltaTime);
+		}
+	}
+}
